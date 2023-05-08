84f5ac5c025135869072a443f39170acd2414caa
kelsey.makale <kelsey.makale@interswitchgroup.com>
2023-05-08 13:09:51 +0300

    UPDATE: Modified build.gradle

--- src/main/java/com/interswitchgroup/tx_user_portal/TxUserPortalApplication.java
+++ src/main/java/com/interswitchgroup/tx_user_portal/TxUserPortalApplication.java
@@ -1,11 +1,8 @@
 package com.interswitchgroup.tx_user_portal;
 
-import com.interswitchgroup.tx_user_portal.repositories.OrganizationRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class TxUserPortalApplication {
