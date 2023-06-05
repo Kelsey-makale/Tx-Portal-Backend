9681b5fc1215dea84eb1152462aa5509403396ad
kelsey.makale <kelsey.makale@interswitchgroup.com>
2023-06-05 11:02:09 +0300

    UPDATE & FIXES: 1. Resolved issue TUP-66.

--- src/main/java/com/interswitchgroup/tx_user_portal/services/EmailService.java
+++ src/main/java/com/interswitchgroup/tx_user_portal/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.interswitchgroup.tx_user_portal.services;
 
+import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class EmailService {
@@ -18,16 +21,13 @@ public class EmailService {
     @Async
     public void sendMail(String to, String subject, String body){
         try{
+            MimeMessage msg = mailSender.createMimeMessage();
+            msg.setFrom("sianwamak@gmail.com");
+            msg.setRecipients(MimeMessage.RecipientType.TO,to);
+            msg.setSubject(subject);
+            msg.setText(body, "utf-8", "html");
 
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setTo(to);
-            mailMessage.setSubject(subject);
-            mailMessage.setText(body);
-            mailMessage.setFrom("sianwamak@gmail.com"); //todo: get proper email
-
-            mailSender.send(mailMessage);
-
-
+            mailSender.send(msg);
         }catch(Exception e){
             LOGGER.error("Failed to send email", e);
             throw new IllegalStateException("Failed to send email");
@@ -38,15 +38,14 @@ public class EmailService {
     @Async
     public void sendMailWithCC(String to, String subject, String body, String[] cc){
         try{
+            MimeMessage msg = mailSender.createMimeMessage();
+            msg.setFrom("sianwamak@gmail.com");
+            msg.setRecipients(MimeMessage.RecipientType.TO,to);
+            msg.setRecipients(MimeMessage.RecipientType.CC, Arrays.toString(cc));
+            msg.setSubject(subject);
+            msg.setText(body, "utf-8", "html");
 
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setTo(to);
-            mailMessage.setSubject(subject);
-            mailMessage.setText(body);
-            mailMessage.setCc(cc);
-            mailMessage.setFrom("support.ke@quickteller.com"); //todo: get proper email
-
-            mailSender.send(mailMessage);
+            mailSender.send(msg);
 
         }catch(Exception e){
             LOGGER.error("Failed to send email", e);
